d911d3307e8145dc0d399b3ae359197f858991d7
Mike Childs <mike@mjcdev.co.uk>
2019-02-11 11:58:35 +0000

    Basic Swagger Integration

--- src/ESFA.DC.Summarisation.API/Startup.cs
+++ src/ESFA.DC.Summarisation.API/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace ESFA.DC.Summarisation.API
 {
@@ -26,6 +27,11 @@ namespace ESFA.DC.Summarisation.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Summarised Actuals", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +47,17 @@ namespace ESFA.DC.Summarisation.API
             }
 
             app.UseHttpsRedirection();
+
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Summarised Actuals v1");
+                c.RoutePrefix = string.Empty;
+            });
+
+
             app.UseMvc();
         }
     }
