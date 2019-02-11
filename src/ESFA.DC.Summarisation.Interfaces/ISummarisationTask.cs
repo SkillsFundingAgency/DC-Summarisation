a261bf95805788753a4fddfc9da6fffa1fab2a87
kelvin.morris <kelvin.morris@eas.co.uk>
2019-02-11 11:12:16 +0000

    #68988 Creating basic Console App, added initial Interfaces project

---
+++ src/ESFA.DC.Summarisation.Interfaces/ISummarisationTask.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace ESFA.DC.Summarisation.Interfaces
+{
+    public interface ISummarisationTask
+    {
+        string TaskName { get; }
+
+        Task ExecuteAsync(CancellationToken cancellationToken);
+    }
+}
