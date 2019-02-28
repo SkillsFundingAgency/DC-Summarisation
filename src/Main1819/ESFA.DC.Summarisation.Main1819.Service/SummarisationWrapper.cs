3253a4a230acd0fa142b4c04ec4b149e716072cd
Anil Pabbati <anil.pabbati@outlook.com>
2019-02-28 15:45:21 +0000

    WIP

--- src/Main1819/ESFA.DC.Summarisation.Main1819.Service/SummarisationWrapper.cs
+++ src/Main1819/ESFA.DC.Summarisation.Main1819.Service/SummarisationWrapper.cs
@@ -1,19 +1,50 @@
-﻿using System;
+﻿using ESFA.DC.Summarisation.Configuration;
+using ESFA.DC.Summarisation.Data.External.FCS.Interface;
+using ESFA.DC.Summarisation.Data.Input.Interface;
+using ESFA.DC.Summarisation.Data.Repository.Interface;
+using ESFA.DC.Summarisation.Interfaces;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading;
 
 namespace ESFA.DC.Summarisation.Main1819.Service
 {
     public class SummarisationWrapper
     {
-        public SummarisationWrapper()
-        {
+        private readonly List<IProvider> _providers;
+        private readonly List<FundingType> _fundingTypes;
+        private readonly IFcsRepository _fcsRepository;
 
+        private IReadOnlyDictionary<string, IReadOnlyCollection<IFcsContractAllocation>> _fcsContractAllocations;
+
+        public SummarisationWrapper(List<FundingType> fundingTypes,
+                                    IFcsRepository fcsRepository,
+                                    List<IProvider> providers)
+        {
+            _providers = providers;
+            _fundingTypes = fundingTypes;
+            _fcsRepository = fcsRepository;
         }
 
-        public void Summarise()
+        public async void Summarise()
         {
+            _fcsContractAllocations = await _fcsRepository.RetrieveAsync(CancellationToken.None);
+            foreach (var fundingType in _fundingTypes)
+            {
+                foreach (var provider in _providers)
+                {
+                    if (_fcsContractAllocations["fspc"].Any(x => x.DeliveryUkprn == provider.UKPRN))
+                    {
+                        ISummarisationService summarisationService = new SummarisationService();
 
+                        summarisationService.Summarise(fundingType, provider);
+                    }
+                }
+            }
         }
+
+      
     }
 }
