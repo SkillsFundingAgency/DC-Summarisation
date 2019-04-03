a8b82ad34cd2aff8842bad53d102760d20b1b54a
kelvin.morris <kelvin.morris@eas.co.uk>
2019-04-03 13:08:22 +0100

    WIP

---
+++ src/ESF.DC.Summarisation.Main1819.Data/Repository/EasRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Threading;
+using System.Threading.Tasks;
+using ESFA.DC.Summarisation.Data.Input.Interface;
+using ESFA.DC.Summarisation.Interface;
+using Dapper;
+
+namespace ESF.DC.Summarisation.Main1819.Data.Repository
+{
+    public class EasRepository : IProviderRepository
+    {
+        public string FundModel { get; }
+
+        private readonly Func<SqlConnection> _sqlConnectionFactory;
+
+        public EasRepository(Func<SqlConnection> sqlConnectionFactory)
+        {
+            _sqlConnectionFactory = sqlConnectionFactory;
+        }
+
+        public Task<IReadOnlyCollection<IProvider>> RetrieveProvidersAsync(int pageSize, int pageNumber, CancellationToken cancellationToken)
+        {
+            using (var connection = _sqlConnectionFactory.Invoke())
+            {
+            }
+        }
+
+        public Task<int> RetrieveProviderPageCountAsync(int pageSize, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- src/ESF.DC.Summarisation.Main1819.Data/Repository/Fm35Repository.cs
+++ src/ESF.DC.Summarisation.Main1819.Data/Repository/Fm35Repository.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.IO.Compression;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
