1bef95aa8f9946f7e2ded2b233327fb6895fdca0
Anil Pabbati <anil.pabbati@outlook.com>
2019-08-01 16:35:22 +0100

    Configuration based on the spec v3.0

---
+++ src/Apps1920/ESFA.DC.Summarisation.Apps1920.Service.Tests/CollectionPeriodTests.cs
@@ -0,0 +1,40 @@
+﻿using ESFA.DC.Serialization.Json;
+using FluentAssertions;
+using Xunit;
+
+namespace ESFA.DC.Summarisation.Apps1920.Service.Tests
+{
+    public class CollectionPeriodTests
+    {
+        [Fact]
+        public void CollectionPeriodCount()
+        {
+            NewProvider().Provide().Should().HaveCount(12);
+        }
+
+        [Theory]
+        [InlineData(1, 8, 2019, 201908)]
+        [InlineData(2, 9, 2019, 201909)]
+        [InlineData(3, 10, 2019, 201910)]
+        [InlineData(4, 11, 2019, 201911)]
+        [InlineData(5, 12, 2019, 201912)]
+        [InlineData(6, 1, 2020, 202001)]
+        [InlineData(7, 2, 2020, 202002)]
+        [InlineData(8, 3, 2020, 202003)]
+        [InlineData(9, 4, 2020, 202004)]
+        [InlineData(10, 5, 2020, 202005)]
+        [InlineData(11, 6, 2020, 202006)]
+        [InlineData(12, 7, 2020, 202007)]
+        public void CollectionPeriodConfig(int period, int calendarMonth, int calendarYear, int actualsSchemaPeriod)
+        {
+            var collectionPeriods = NewProvider().Provide();
+
+            collectionPeriods.Should().Contain(cp => cp.Period == period && cp.CalendarMonth == calendarMonth && cp.CalendarYear == calendarYear && cp.ActualsSchemaPeriod == actualsSchemaPeriod);
+        }
+
+        private CollectionPeriodsProvider NewProvider()
+        {
+            return new CollectionPeriodsProvider(new JsonSerializationService());
+        }
+    }
+}
