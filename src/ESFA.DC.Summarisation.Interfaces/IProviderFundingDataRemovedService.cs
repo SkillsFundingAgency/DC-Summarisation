49ba0be33de0946a91e5499efe3e91423cbcc878
Anil Pabbati <anil.pabbati@outlook.com>
2020-01-14 16:24:26 +0000

    ProviderFundingDataRemoved service added

---
+++ src/ESFA.DC.Summarisation.Interfaces/IProviderFundingDataRemovedService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using ESFA.DC.Summarisation.Data.Output.Model;
+
+namespace ESFA.DC.Summarisation.Interfaces
+{
+    public interface IProviderFundingDataRemovedService
+    {
+        Task<IEnumerable<SummarisedActual>> FundingDataRemovedAsync(List<SummarisedActual> providerActuals, ISummarisationMessage summarisationMessage, CancellationToken cancellationToken);
+    }
+}
