6ac7e676e8e867fc2a823c95542e343d4df8178e
Ashley Beecham <ash@momentoussolutions.co.uk>
2020-01-29 14:46:17 +0000

    92432 - Updates following PR.

--- src/Generic/ESFA.DC.Summarisation.Generic.Modules/GenericCollectionModule.cs
+++ src/Generic/ESFA.DC.Summarisation.Generic.Modules/GenericCollectionModule.cs
@@ -2,6 +2,7 @@
 using ESFA.DC.GenericCollection.EF;
 using ESFA.DC.GenericCollection.EF.Interface;
 using ESFA.DC.Summarisation.Constants;
+using ESFA.DC.Summarisation.Data.Repository;
 using ESFA.DC.Summarisation.Generic.Interfaces;
 using ESFA.DC.Summarisation.Generic.Service;
 using ESFA.DC.Summarisation.Interfaces;
@@ -28,6 +29,8 @@ namespace ESFA.DC.Summarisation.Apps.Modules
             var dataStoreEntitiesConfig = BuildDbContextOptions<GenericCollectionContext>(_summarisationDataOptions.GenericCollectionsConnectionString, _summarisationDataOptions.SqlCommandTimeoutSeconds);
 
             containerBuilder.Register(c => new GenericCollectionContext(dataStoreEntitiesConfig)).As<IGenericCollectionContext>().ExternallyOwned();
+
+            containerBuilder.RegisterType<GenericCollectionRepository>().As<IGenericCollectionRepository>();
         }
 
         private DbContextOptions<T> BuildDbContextOptions<T>(string connectionString, string sqlCommandTimeOut) where T : DbContext
