3bf8de45d6d3201dfaa29a84da4242012f9b570d
Anil Pabbati <anil.pabbati@outlook.com>
2020-02-21 14:30:11 +0000

    Mapper tests added

---
+++ src/ESFA.DC.Summarisation.Data.BAU.Persist.Tests/EventLogMapperTests.cs
@@ -0,0 +1,37 @@
+﻿using ESFA.DC.Summarisation.Data.BAU.Persist.Interfaces;
+using ESFA.DC.Summarisation.Data.BAU.Persist.Mapper;
+using ESFA.DC.Summarisation.Data.BAU.Persist.Model;
+using ESFA.DC.Summarisation.Service.Model;
+using FluentAssertions;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ESFA.DC.Summarisation.Data.BAU.Persist.Tests
+{
+    public class EventLogMapperTests
+    {
+        [Fact]
+        public void MapTest()
+        {
+            var collectionReturnMock = new CollectionReturn()
+            {
+                CollectionType = "ILR",
+                CollectionReturnCode = "R01"
+            };
+
+            var result = Mapper().Map(collectionReturnMock);
+
+            result.CollectionType.Should().BeEquivalentTo("ILR");
+            result.CollectionReturnCode.Should().BeEquivalentTo("R01");
+
+        }
+
+        private IEventLogMapper Mapper() => new EventLogMapper();
+
+    }
+}
