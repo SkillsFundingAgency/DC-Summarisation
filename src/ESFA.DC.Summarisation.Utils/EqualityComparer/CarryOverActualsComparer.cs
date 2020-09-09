b9f76451125fa88e3cf5759f9f79430cb97b7a81
Anil Pabbati <anil.pabbati@outlook.com>
2020-09-09 15:46:27 +0100

    Code review comment to retvert the Hascode changes.

--- src/ESFA.DC.Summarisation.Utils/EqualityComparer/CarryOverActualsComparer.cs
+++ src/ESFA.DC.Summarisation.Utils/EqualityComparer/CarryOverActualsComparer.cs
@@ -17,8 +17,19 @@ namespace ESFA.DC.Summarisation.Utils.EqualityComparer
         }
 
         public int GetHashCode(SummarisedActual obj)
-        {
-            return 0;
+        {   // Check whether the object is null. 
+            if (obj == null)
+            {
+                return 0;
+            }
+
+            // Not hashing OrganisationId as usage of this is pre-filtered to be with same UKPRN, fields included are the key for Carry Over Actuals
+
+            return (obj.OrganisationId,
+                obj.DeliverableCode,
+                obj.Period,
+                obj.FundingStreamPeriodCode,
+                obj.ContractAllocationNumber).GetHashCode();
         }
     }
 }
