d1577875564882497747713a6a66f877e196c388
Anil Pabbati <anil.pabbati@outlook.com>
2020-09-09 12:20:26 +0100

    104826 ESF Summarisation - not summarising specific £0 values previously summarised

--- src/ESFA.DC.Summarisation.Utils/EqualityComparer/CarryOverActualsComparer.cs
+++ src/ESFA.DC.Summarisation.Utils/EqualityComparer/CarryOverActualsComparer.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using ESFA.DC.Summarisation.Service.Model;
 
 namespace ESFA.DC.Summarisation.Utils.EqualityComparer
@@ -7,24 +8,17 @@ namespace ESFA.DC.Summarisation.Utils.EqualityComparer
     {
         public bool Equals(SummarisedActual x, SummarisedActual y)
         {
-            return GetHashCode(x) == GetHashCode(y);
+            return x.DeliverableCode == y.DeliverableCode
+                   && x.Period == y.Period
+                   && x.FundingStreamPeriodCode.Equals(y.FundingStreamPeriodCode, StringComparison.OrdinalIgnoreCase)
+                   && x.ContractAllocationNumber.Equals(y.ContractAllocationNumber, StringComparison.OrdinalIgnoreCase)
+                   && x.OrganisationId.Equals(y.OrganisationId, StringComparison.OrdinalIgnoreCase);
+
         }
 
         public int GetHashCode(SummarisedActual obj)
         {
-            // Check whether the object is null. 
-            if (obj == null)
-            {
-                return 0;
-            }
-
-            // Not hashing OrganisationId as usage of this is pre-filtered to be with same UKPRN, fields included are the key for Carry Over Actuals
-
-            return (obj.OrganisationId,
-                obj.DeliverableCode,
-                obj.Period,
-                obj.FundingStreamPeriodCode,
-                obj.ContractAllocationNumber).GetHashCode();
+            return 0;
         }
     }
 }
